9f4fac8596ab043ab91575d457249f495ac7aac4
BlazingRockStorm <rocker.gryphon@gmail.com>
2022-05-12 15:20:02 +0900

    add code

--- Handler.cs
+++ Handler.cs
@@ -1,5 +1,6 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
+using Microsoft.Health.Fhir.Anonymizer.Core;
 
 [assembly:LambdaSerializer(
   typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -9,9 +10,13 @@ namespace AwsDotnetCsharp{
   {
     public APIGatewayProxyResponse HandlerR4(APIGatewayProxyRequest proxyRequest)
     {
+      AnonymizerEngine engine = new AnonymizerEngine("r4-configuration-sample.json");
+
+      var output = engine.AnonymizeJson(proxyRequest.Body, null);
+
       return new APIGatewayProxyResponse
       {
-        Body = proxyRequest.Body,
+        Body = output,
         StatusCode = 200,
       };
     }
