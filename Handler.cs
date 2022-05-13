37ce86a3da7c7c1603d4f450464aea4e33621298
BlazingRockStorm <rocker.gryphon@gmail.com>
2022-05-13 13:58:32 +0900

    working code

--- Handler.cs
+++ Handler.cs
@@ -1,7 +1,11 @@
+using System;
+using System.IO;
+using System.Text;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using Microsoft.Health.Fhir.Anonymizer.Core;
 using Microsoft.Health.Fhir.Anonymizer.Core.AnonymizerConfigurations;
+using Newtonsoft.Json;
 
 [assembly:LambdaSerializer(
   typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -11,13 +15,14 @@ namespace AwsDotnetCsharp{
   {
     public APIGatewayProxyResponse HandlerR4(APIGatewayProxyRequest proxyRequest)
     {
-      // from FHIR\src\Microsoft.Health.Fhir.Anonymizer.Shared.Core.UnitTests\AnonymizerEngineTests.cs
-      AnonymizerEngine engine = new AnonymizerEngine("r4-configuration.json");
-      var settings = new AnonymizerSettings()
-        {
-          IsPrettyOutput = true
-        };
-      var output = engine.AnonymizeJson(proxyRequest.Body, settings);
+      var configContext = File.ReadAllText("r4-configuration.json", Encoding.UTF8);
+      LambdaLogger.Log("r4-configuration.json: " + JsonConvert.SerializeObject(configContext));
+
+      AnonymizerEngine.InitializeFhirPathExtensionSymbols();
+
+      var configurationManager = AnonymizerConfigurationManager.CreateFromSettingsInJson(configContext);
+      var engine = new AnonymizerEngine(configurationManager);
+      var output = engine.AnonymizeJson(proxyRequest.Body);
 
       return new APIGatewayProxyResponse
       {
